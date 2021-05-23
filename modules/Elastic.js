640622ec36aea44f32359eb8481a5f94fa7fa1ad
johanHiguita <higuitajohan@gmail.com>
2021-05-22 20:06:55 -0500

    insertDocument method

--- modules/Elastic.js
+++ modules/Elastic.js
@@ -2,19 +2,24 @@ const axios = require("axios");
 const baseURL = "http://127.0.0.1:9200";
 
 class Elastic {
-
   static getMapping = (index) => {
-
     try {
       const response = axios.get(`${baseURL}/movies/_mapping`);
       return response;
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
+  //insert document given it the id
+  static insertDocument = (id, document) => {
+    try {
+      const response = axios.post(`${baseURL}/movies/_doc/${id}`, document);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 }
 
 module.exports = Elastic;
-
-
--- tests.js
+++ tests.js
@@ -5,9 +5,21 @@ const print = (data) => {
     console.log(util.inspect(data, false, null, true /* enable colors */));
 }
 
+
+/************************ tests *********************/
+const doc = {
+    "genre" : ["IMAX", "Action"],
+    "title": "Johan Movie",
+    "year": 1992
+}
 Elastic
-    .getMapping("movies")
+    .insertDocument("100", doc)
     .then(resp => print(resp.data));
+
+
+/* Elastic
+    .getMapping("movies")
+    .then(resp => print(resp.data)); */
 
 
 
